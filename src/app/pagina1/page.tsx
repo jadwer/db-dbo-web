9a9652d1f7f3fdad601a010ffb2e1c5470e33c23
Gabino Ramírez <jadwer@msn.com>
2024-05-11 20:54:29 -0600

    creating templates for Designers

---
+++ src/app/pagina1/page.tsx
@@ -0,0 +1,10 @@
+import Template from './pagina1'
+
+function testPage () {
+    const text = "some text";
+  return (
+    <Template text={text}/>
+  )
+}
+
+export default testPage 
---
+++ src/app/pagina1/pagina1.tsx
@@ -0,0 +1,12 @@
+const Template = (props: any) => (
+
+    <main>
+    <div>testPage
+        <p>{props.text}</p>
+    </div>
+    </main>
+
+
+
+); export default Template;
+    
